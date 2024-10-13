78b5c7a2b5112de9dd0067185ffc2b7f82e06a71
Luis Anibal <aniballuis180@gmail.com>
2024-10-13 15:08:33 -0400

    Añadide función de identificación de variables y coeficientes

--- Asignacion04.py
+++ Asignacion04.py
@@ -1,2 +1,20 @@
 import numpy as np
 import scipy as sp
+
+def leerInput (stringinput):
+    if 'x' in stringinput:
+        posicionX = stringinput.find('x')
+        a = stringinput[0:posicionX]
+    if 'y' in stringinput:
+        posicionY = stringinput.find('y')
+        b = stringinput[0:posicionY]
+    if 'z' in stringinput:
+        posicionZ = stringinput.find('z')
+        c = stringinput[0:posicionZ]
+    if 'w' in stringinput:
+        posicionW = stringinput.find('w')
+        d = stringinput[0:posicionW]
+
+
+ecuacion = input("Administre la ecuación en cuestión con el formato correcto: ")
+leerInput(ecuacion.lower())
