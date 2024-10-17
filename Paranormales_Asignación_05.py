0590d6249edb61587b59b4633fa492ebf2061d85
Luis Anibal <aniballuis180@gmail.com>
2024-10-17 11:53:13 -0400

    Terminado la primera parte de la tarea

--- Paranormales_Asignación_05.py
+++ Paranormales_Asignación_05.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 class Cola:
 #Definir métodos que nos provean con las operaciones de la cola
     
@@ -40,7 +42,84 @@ class Cola:
         return (self.data == [])
     
 cola1 = Cola()
-cola1.enqueue(1)
-print(cola1)
-print(repr(cola1))
-print(len(cola1))
+cola2 = Cola()
+cola3 = Cola()
+
+#No pueden haber más de 5 clientes en una fila a la vez
+numMaxClientes = 5
+
+while(True):
+    try:
+        numClientes = int(input("Provea el número de clientes que asistirán este día: "))
+        if(numClientes < 0):
+            raise ValueError
+        break
+    except:
+        print("Asegúrese que el número de personas sea un número natural.")
+
+for indiceCliente in range (numClientes):
+
+    numEntrada = randrange(1, 4)
+    while(True):
+        if numEntrada == 1:
+            if len(cola1) < numMaxClientes:
+                cola1.enqueue(indiceCliente+1)
+                print("Se añadió un cliente a la fila 1")
+                break
+            else:
+                print("Fila 1 está llena, asignando cliente a otra fila...")
+                numEntrada = randrange(2,4)
+                continue
+        elif numEntrada == 2:
+            if len(cola2) < numMaxClientes:
+                cola2.enqueue(indiceCliente+1)
+                print("Se añadió un cliente a la fila 2")
+                break
+            else:
+                print("Fila 2 está llena, asignando cliente a otra fila...")
+                numEntrada = randrange(1,4,2)
+                continue
+        elif numEntrada == 3:
+            if len(cola3) < numMaxClientes:
+                cola3.enqueue(indiceCliente+1)
+                print("Se añadió un cliente a la fila 3")
+                break
+            else:
+                print("Fila 3 está llena, asignando cliente a otra fila...")
+                numEntrada = randrange(1,3)
+                continue
+
+    print(f"Hay {len(cola1)} cliente{'s' if len(cola1) != 1 else ''} en la fila 1: {cola1}")
+    print(f"Hay {len(cola2)} cliente{'s' if len(cola2) != 1 else ''} en la fila 2: {cola2}")
+    print(f"Y hay {len(cola3)} cliente{'s' if len(cola3) != 1 else ''} en la fila 3: {cola3}")
+
+    numSalida = randrange(1, 4)
+    if numSalida == 1:
+        try:
+            print(f"Cliente {cola1.peek()} atendido en fila 1.\n")
+            cola1.dequeue()
+        except:
+            print("Se intentó atender un cliente en la fila 1, pero está vacía.\n")
+    elif numSalida == 2:
+        try:
+            print(f"Cliente {cola2.peek()} atendido en fila 2.\n")
+            cola2.dequeue()
+        except:
+            print("Se intentó atender un cliente en la fila 2, pero está vacía.\n")
+    elif numSalida == 3:
+        try:
+            print(f"Cliente {cola3.peek()} atendido en fila 3.\n")
+            cola3.dequeue()
+        except:
+            print("Se intentó atender un cliente en la fila 3, pero está vacía.\n")
+
+print("El día ha llegado a su fin y ya no se admieten más clientes, vaciando las filas restantes...")
+while(not cola1.isEmpty()):
+    print(f"Atendido cliente {cola1.peek()} en la fila 1")
+    cola1.dequeue()
+while(not cola2.isEmpty()):
+    print(f"Atendido cliente {cola2.peek()} en la fila 2")
+    cola2.dequeue()
+while(not cola3.isEmpty()):
+    print(f"Atendido cliente {cola3.peek()} en la fila 3")
+    cola3.dequeue()
