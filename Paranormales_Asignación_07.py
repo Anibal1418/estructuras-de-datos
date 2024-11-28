79abeb411cb92699c85f030390ea858210fd8e78
Luis Anibal <aniballuis180@gmail.com>
2024-11-28 13:39:24 -0400

    Hecho soluciones para los ejercicios 1 y 2

--- Paranormales_Asignación_07.py
+++ Paranormales_Asignación_07.py
@@ -9,34 +9,76 @@ class ColaNotificaciones:
 
     # Construye una cola vacía
     def __init__(self):
-        self.data = []
+        self.notis = []
 
-    # Representa el contenido de la cola
+    # Representa el contenido de la cola para imprimir
     def __repr__(self):
-        return " | ".join(map(str, self.data))
+        return "\n".join(map(str, self.notis))
+    '''Favor buscar una forma para imprimir la cola de manera más estética'''
 
     # Devuelve la longitud o número de elementos de la cola
     def __len__(self):
-        return len(self.data)
+        return len(self.notis)
     
     # Añade un elemento a la cola
-    def enqueue(self, e):
-        self.data.append(e)
+    def enqueue(self, time, app, message):
+        self.notis.append({"hora":time, "aplicación":app, "mensaje":message})
 
     # Extrae y elimina un elemento de la cola
     def dequeue(self):
         if self.isEmpty(): # Sube un error en caso de que esté vacía
             raise IndexError('Cola vacía')
         else:
-            return self.data.pop(0)
+            return self.notis.pop(0)
 
     # Devuelve el primer elemento de la cola sin eliminarlo
     def peek(self):
         if self.isEmpty():
             raise IndexError('Cola vacía')
         else:
-            return self.data[0]
+            return self.notis[0]
 
     # Devuelve True si la cola está vacía
     def isEmpty(self):
-        return self.data == []
+        return self.notis == []
+    
+    # Implementación del primer ejercicio, función que borra todas las notificaciones de Facebook
+    def borrarNotisFacebook(self):
+
+        ilist = [] # Crea una lista que almacenará los índices de mensajes que provienen de Facebook
+        for i in range(0, self.__len__()):
+            if(self.notis[i]["aplicación"] == "Facebook"):
+                ilist.append(i)
+
+        # Elimina los elementos en los índices almacenados en modo reversa para que no nos dé un IndexError
+        for element in reversed(ilist):
+            self.notis.pop(element)
+
+    # Implementación del segundo ejercicio, función que muestra las notificaciones de Twitter con "Python" en su mensaje
+    def verNotisTwitter(self):
+        for i in range (0, self.__len__()):
+            if(self.notis[i]["aplicación"] == "Twitter" and ("Python" in self.notis[i]["mensaje"])):
+                print(self.notis[i])
+
+
+cola = ColaNotificaciones()
+
+cola.enqueue("15:35", "Facebook", "Un amigo te ha enviado un mensaje")
+cola.enqueue("16:02", "Twitter", "Python Account ha publicado un tweet")
+cola.enqueue("16:23", "Instagram", "User ha subido una foto después de mucho tiempo")
+cola.enqueue("16:24", "Facebook", "Un amigo te ha enviado un mensaje")
+cola.enqueue("17:30", "Facebook", "Anuncio: nuevo juego móvil")
+cola.enqueue("17:35", "Tiktok", "User te ha enviado un mensaje")
+cola.enqueue("18:00", "Youtube", "KSI ha subido un video")
+cola.enqueue("19:23", "Youtube", "MrBeast ha subido un video")
+cola.enqueue("20:20", "Twitter", "User ha publicado un tweet")
+cola.enqueue("21:59", "Instagram", "User te ha enviado un reel")
+cola.enqueue("22:35", "Twitter", "Python Account ha publicado un tweet")
+
+print(cola) # Cola antes
+cola.borrarNotisFacebook()
+print('\n')
+print(cola) # Cola después
+
+print('\n')
+cola.verNotisTwitter() # Imprime por si mismo los elementos deseados
