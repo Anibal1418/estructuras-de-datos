b210c5a32e5598152b20a8f6394d714e3fb62041
Luis Anibal <aniballuis180@gmail.com>
2024-12-04 17:13:32 -0400

    Continuado con la clase GPS

--- Paranormales_Proyecto_Integrador_2.py
+++ Paranormales_Proyecto_Integrador_2.py
@@ -6,21 +6,31 @@ Cada nodo también tiene una lista llamada connections,
 que se utiliza para almacenar los bordes que están conectados
 a este nodo.
 '''
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.connections = []
-
+class Interseccion:
+    def __init__(self, id):
+        self.id = id
+        self.conexiones = []
+    
+def calcDistancia (origen, destino):
+    for conexionA in origen.conexiones: 
+        if conexionA.destination == destino:
+            return conexionA.distancia
+        elif conexionA.origen == destino:
+            return conexionA.distancia
+    return None
+    
 '''
 La clase Edge representa una artista en el grafo. 
 Cada arista tiene dos partes: un nodo de origen (origin) 
 y un nodo de destino (destination), lo que significa que  
 la arista va desde el nodo de origen hasta el nodo de destino.
 '''
-class Edge:
-    def __init__(self, origin, destination):
-        self.origin = origin
+class Calle:
+    def __init__(self, name, origin, destination, kilometraje):
+        self.name = name
+        self.origen = origin
         self.destination = destination
+        self.distancia = kilometraje
 
 '''
 Se debe buscar una manera para interrelacionar estas clases de manera que sean parte de un solo grafo llamado GPS
@@ -28,30 +38,35 @@ en el cual podemos hacer funciones de búsqueda y conexión.
 '''
 
 # Ejemplo de Construcción
-node_a = Node("A")
-node_b = Node("B")
+interseccion_a = Interseccion("A")
+interseccion_b = Interseccion("B")
 
-edge_ab = Edge(node_a, node_b)
+calle_ab = Calle("Máximo Gómez", interseccion_a, interseccion_b, 10)
 
 # Añadir la arista tanto al nodo A como al nodo B
-node_a.connections.append(edge_ab)
-node_b.connections.append(edge_ab)
+interseccion_a.conexiones.append(calle_ab)
+interseccion_b.conexiones.append(calle_ab)
+
+# Calcular la distancia de la calle común que tienen both ways
+print(calcDistancia(interseccion_a, interseccion_b))
+print(calcDistancia(interseccion_b, interseccion_a))
+
 
 # Draft de clase que podría agregar grafos y aristas
 
-class Grafo(object):
+class GPS:
+
     def __init__(self):
-        self.relaciones = {}
+        self.intersecciones = []
 
-def __str__(self):
-    return str(self.relaciones)
- 
-def agregar(grafo, nodo):
-    grafo.relaciones.update({nodo:[]})
+    def __str__(self):
+        return str(self.intersecciones)
+    
+    def agregar(self, interseccion):
+        self.intersecciones.append(interseccion)
 
-def relacionar(grafo, elemento1, elemento2):
-    relacionarUnilateral(grafo, elemento1, elemento2)
-    relacionarUnilateral(grafo, elemento2, elemento1)
-   
-def relacionarUnilateral(grafo, origen, destino):
-    grafo.relaciones[origen].append(destino)
+
+gps = GPS()
+
+gps.agregar(interseccion_a)
+gps.agregar(interseccion_b)
