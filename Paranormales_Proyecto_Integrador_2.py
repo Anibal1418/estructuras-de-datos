078cbdcfb49db4646606f6789ede80aeadaf1847
Luis Anibal <aniballuis180@gmail.com>
2024-12-08 12:04:25 -0400

    Actualizado las clases

--- Paranormales_Proyecto_Integrador_2.py
+++ Paranormales_Proyecto_Integrador_2.py
@@ -1,9 +1,9 @@
 '''
-La clase Node representa un nodo en el grafo. 
+La clase Interseccion representa un nodo en el grafo. 
 Cada nodo tiene un valor (value) que puede ser cualquier tipo 
 de dato (por ejemplo, una cadena de texto). 
-Cada nodo también tiene una lista llamada connections,
-que se utiliza para almacenar los bordes que están conectados
+Cada nodo también tiene una lista llamada conexiones,
+que se utiliza para almacenar las aristas que están conectadas
 a este nodo.
 '''
 class Interseccion:
@@ -12,7 +12,7 @@ class Interseccion:
         self.conexiones = []  
     
 '''
-La clase Edge representa una artista en el grafo. 
+La clase Calle representa una artista en el grafo. 
 Cada arista tiene dos partes: un nodo de origen (origin) 
 y un nodo de destino (destination), lo que significa que  
 la arista va desde el nodo de origen hasta el nodo de destino.
@@ -28,6 +28,7 @@ class Calle:
 Esta son funciones independiente que manipulan nodos y calles
 '''
 
+# Conecta bilateralmente los nodos
 def conectar(origen, calle, destino):
     if calle not in origen.conexiones:
         origen.conexiones.append(calle)
@@ -41,9 +42,12 @@ def estaConectado (origen, destino):
             return True
         elif conexionA.origen == destino:
             return True
+    # En caso de que no estén conectados directamente, chequear por medio de DFS si algun camino lleva al nodo destino
     
     return False
 
+# Función que calcula la distancia entre un nodo de origen y un nodo destino, hasta ahora solo hace un
+# Chequeo superficial, necesitamos que haga un chequeo profundo en caso de que los dos nodos no estén conectados directamente
 def calcDistancia (origen, destino):
     for conexionA in origen.conexiones: 
         if conexionA.destination == destino:
@@ -66,23 +70,26 @@ conectar(interseccion_a, calle_ab, interseccion_b)
 conectar(interseccion_b, calle_bc, interseccion_c)
 
 # Calcular la distancia entre dos intersecciones y las calles que las une
+# Ambas deben imprimir 10, debido a que están conectadas por medio de arista de 10 de longitud
 print(calcDistancia(interseccion_a, interseccion_b))
 print(calcDistancia(interseccion_b, interseccion_a))
 
 
-# Draft de clase que podría agregar grafos y aristas
+# ESTA ES LA CLASE GPS. Básicamente puede ser o una lista de nodos, o un diccionario de nodos accedidos por nombre.
+# Aquí es adonde se van a administrar las funciones de nodos conectados, como búsqueda de caminos, cálculos de distancias, etc.
 
+# Hasta ahora lo que hice fue crear un diccionario, donde cada intersección está colocada de la manera: {interseccion:[calles]}
 class GPS:
 
     def __init__(self):
-        self.intersecciones = []
+        self.intersecciones = {}
 
     def __str__(self):
         return str(self.intersecciones)
     
     def agregar(self, interseccion):
         if interseccion not in self.intersecciones:
-            self.intersecciones.append(interseccion)
+            self.intersecciones[interseccion] = [interseccion.conexiones]
         
     '''CONSTRUIR una funcion que devuelva todos los caminos entre dos nodos'''
     # def findPaths(self, start, end):
@@ -94,7 +101,7 @@ class GPS:
 
     # Consulta si el grafo está vacío
     def isEmpty(self):
-        return self.intersecciones == []
+        return self.intersecciones == {}
 
     # Consulta si la interseccion existe en el grafo
     def existeInterseccion(self, interseccion):
@@ -110,16 +117,15 @@ class GPS:
 
 
 gps = GPS()
-#print(gps.isEmpty())
 
 gps.agregar(interseccion_a)
 gps.agregar(interseccion_b)
 
-#print(gps.isEmpty())
 
-#print(gps.existeInterseccion(interseccion_a))
-#print(gps.existeCalle(calle_ab))
-
+# Estos statements de aquí abajo deberían imprimir true todos, pero el último imprime false ya que la función
+# estaConectado no hace una búsqueda de profundidad
+print(gps.existeInterseccion(interseccion_a))
+print(gps.existeCalle(calle_ab))
 print(estaConectado(interseccion_a, interseccion_b))
 print(estaConectado(interseccion_b, interseccion_c))
 print(estaConectado(interseccion_a, interseccion_c))
